fc56afef47056ac5bfc7d53e758f3cfc9af9d946
Yash Shah <90525149+gityash56@users.noreply.github.com>
2022-10-21 15:27:07 +0530

    ChkNum_Eql_or_Not
    Number chk Equal or Not.!!

---
+++ chk_Equal_or_note.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int int1, int2;
+	clrscr();
+ 
+    printf("Input the values for Number1 and Number2 : ");
+    scanf("%d %d", &int1, &int2);
+
+    if (int1 == int2)
+    {
+        printf("Number1 and Number2 are equal\n");
+    }
+    else
+        printf("Number1 and Number2 are not equal\n");
+	getch();
+}
